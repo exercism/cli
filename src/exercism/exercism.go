db77189208cbb2e31f8e70f651a2bade8064cfd7
Mike Gehard <mike.gehard@gmail.com>
2013-08-08 22:08:39 -0700

    Allow login and logout.

--- src/exercism/exercism.go
+++ src/exercism/exercism.go
@@ -1,9 +1,28 @@
 package exercism
 
 import (
-	"fmt"
+	"encoding/json"
+	"io/ioutil"
+	"os"
 )
 
-func Login() {
-	fmt.Println("Hello from login")
+const FILENAME = ".exercism"
+
+func Login(configDir string, config Config) (err error) {
+	contents, err := json.Marshal(config)
+
+	if err != nil {
+		return err
+	}
+
+	err = ioutil.WriteFile(createFilename(configDir), contents, 0644)
+	return err
+}
+
+func Logout(configDir string) {
+	os.Remove(createFilename(configDir))
+}
+
+func createFilename(dir string) string {
+	return dir + "/" + FILENAME
 }
--- src/exercism/exercism_test.go
+++ src/exercism/exercism_test.go
@@ -1,10 +1,62 @@
 package exercism
 
 import (
+	"encoding/json"
 	"github.com/stretchr/testify/assert"
+	"io/ioutil"
+	"os"
 	"testing"
 )
 
-func TestPasses(t *testing.T) {
-	assert.True(t, true)
+func assertFileExists(t *testing.T, filename string) {
+	_, err := os.Stat(filename)
+
+	if err != nil {
+		t.Errorf("File [%s] does not exist.", filename)
+	}
+}
+
+func asserFileDoesNotExist(t *testing.T, filename string) {
+	_, err := os.Stat(filename)
+
+	if err == nil {
+		t.Errorf("File [%s] already exist.", filename)
+	}
+}
+
+func TestLoginCreatesConfigFile(t *testing.T) {
+	tmpDir, err := ioutil.TempDir("", "")
+	assert.NoError(t, err)
+
+	filename := tmpDir + "/" + FILENAME
+	asserFileDoesNotExist(t, filename)
+
+	config := Config{"githubUser", "MyApiKey", "/my/exercism/directory"}
+
+	Login(tmpDir, config)
+
+	assertFileExists(t, filename)
+
+	contents, err := ioutil.ReadFile(filename)
+	assert.NoError(t, err)
+
+	configEntries := &Config{}
+	err = json.Unmarshal(contents, configEntries)
+	assert.NoError(t, err)
+
+	assert.Equal(t, configEntries.GithubUsername, "githubUser")
+	assert.Equal(t, configEntries.ApiKey, "MyApiKey")
+	assert.Equal(t, configEntries.ExercismDirectory, "/my/exercism/directory")
+}
+
+func TestLogoutDeletesConfigFile(t *testing.T) {
+	tmpDir, err := ioutil.TempDir("", "")
+	assert.NoError(t, err)
+	filename := tmpDir + "/" + FILENAME
+	err = ioutil.WriteFile(filename, []byte("exercism config\n"), 0644)
+	assert.NoError(t, err)
+
+	Logout(tmpDir)
+
+	asserFileDoesNotExist(t, filename)
 }
