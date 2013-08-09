c5d5252bd8b2c1561da46a47bc02cc12b4eb2af7
Mike Gehard <mike.gehard@gmail.com>
2013-08-09 07:29:55 -0700

    Implement whoami.

--- src/exercism/config.go
+++ src/exercism/config.go
@@ -1,7 +1,42 @@
 package exercism
 
+import (
+	"encoding/json"
+	"io/ioutil"
+)
+
+const FILENAME = ".exercism"
+
+func ConfigFromFile(dir string) (c Config, err error) {
+	bytes, err := ioutil.ReadFile(configFilename(dir))
+	if err != nil {
+		return
+	}
+
+	err = json.Unmarshal(bytes, &c)
+	return
+}
+
+func ConfigToFile(dir string, c Config) (err error) {
+	bytes, err := json.Marshal(c)
+	if err != nil {
+		return
+	}
+
+	err = ioutil.WriteFile(configFilename(dir), bytes, 0644)
+	if err != nil {
+		return
+	}
+
+	return
+}
+
 type Config struct {
 	GithubUsername    string `json:"githubUsername"`
 	ApiKey            string `json:"apiKey"`
 	ExercismDirectory string `json:"exercismDirectory"`
+}
+
+func configFilename(dir string) string {
+	return dir + "/" + FILENAME
 }
