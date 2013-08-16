3daef6e4f6c8866a5862e6963848c5b7396f67bd
Mike Gehard <mike.gehard@gmail.com>
2013-08-15 20:20:43 -0600

    Initial implementation of submitting assignments.

--- src/exercism/api_test.go
+++ src/exercism/api_test.go
@@ -22,7 +22,16 @@ var assignmentsJson = `
 }
 `
 
-var handler = func(rw http.ResponseWriter, r *http.Request) {
+var submitJson = `
+{
+	"status":"saved",
+	"language":"ruby",
+	"exercise":"bob",
+	"submission_path":"/username/ruby/bob"
+}
+`
+
+var fetchHandler = func(rw http.ResponseWriter, r *http.Request) {
 	r.ParseForm()
 	apiKey := r.Form.Get("key")
 	if r.URL.Path != "/api/v1/user/assignments/current" {
@@ -32,6 +41,7 @@ var handler = func(rw http.ResponseWriter, r *http.Request) {
 	}
 	if apiKey != "myApiKey" {
 		rw.WriteHeader(http.StatusForbidden)
+		fmt.Fprintf(rw, `{"error": "Unable to identify user"}`)
 		return
 	}
 
@@ -39,8 +49,28 @@ var handler = func(rw http.ResponseWriter, r *http.Request) {
 	fmt.Fprintf(rw, assignmentsJson)
 }
 
+var submitHandler = func(rw http.ResponseWriter, r *http.Request) {
+	r.ParseForm()
+	apiKey := r.Form.Get("key")
+	if r.URL.Path != "/api/v1/user/assignments" || r.Method != "POST" {
+		fmt.Println("Not found")
+		rw.WriteHeader(http.StatusNotFound)
+		return
+	}
+	if apiKey != "myApiKey" {
+		rw.WriteHeader(http.StatusForbidden)
+		rw.Header().Set("Content-Type", "application/json")
+		fmt.Fprintf(rw, `{"error": "Unable to identify user"}`)
+		return
+	}
+
+	rw.Header().Set("Content-Type", "application/json")
+
+	fmt.Fprintf(rw, submitJson)
+}
+
 func TestFetchWithKey(t *testing.T) {
-	server := httptest.NewServer(http.HandlerFunc(handler))
+	server := httptest.NewServer(http.HandlerFunc(fetchHandler))
 
 	assignments, err := FetchAssignments(server.URL, "myApiKey")
 	assert.NoError(t, err)
@@ -57,12 +87,27 @@ func TestFetchWithKey(t *testing.T) {
 }
 
 func TestFetchWithIncorrectKey(t *testing.T) {
-	server := httptest.NewServer(http.HandlerFunc(handler))
+	server := httptest.NewServer(http.HandlerFunc(fetchHandler))
 
 	assignments, err := FetchAssignments(server.URL, "myWrongApiKey")
 
 	assert.Error(t, err)
 	assert.Equal(t, len(assignments), 0)
+
+	server.Close()
+}
+
+func TestSubmitWithKey(t *testing.T) {
+	server := httptest.NewServer(http.HandlerFunc(submitHandler))
+
+	assignment := Assignment{}
+	response, err := SubmitAssignment(server.URL, "myApiKey", assignment)
+	assert.NoError(t, err)
+
+	assert.Equal(t, response.Status, "saved")
+	assert.Equal(t, response.Language, "ruby")
+	assert.Equal(t, response.Exercise, "bob")
+	assert.Equal(t, response.SubmissionPath, "/username/ruby/bob")
 
 	server.Close()
 }
