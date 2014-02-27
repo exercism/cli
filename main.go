16e8f61197f32158f73bdf9d36912fafcb4d1e7b
Leo Correa <lcorr005@gmail.com>
2014-02-27 16:18:13 -0500

    Allow filtering current by language

--- main.go
+++ main.go
@@ -24,6 +24,13 @@ func main() {
 			ShortName: "c",
 			Usage:     "Show the current assignments",
 			Action: func(c *cli.Context) {
+				var language string
+				argc := len(c.Args())
+				if argc != 0 && argc != 1 {
+					fmt.Println("Usage: exercism current\n   or: exercism current LANGUAGE")
+					return
+				}
+
 				config, err := configuration.FromFile(c.GlobalString("config"))
 				if err != nil {
 					fmt.Println("Are you sure you are logged in? Please login again.")
@@ -35,10 +42,21 @@ func main() {
 					return
 				}
 
-				fmt.Println("Current Assignments")
+				if argc == 1 {
+					language = c.Args()[0]
+					fmt.Println("Current Assignments for", strings.Title(language))
+				} else {
+					fmt.Println("Current Assignments")
+				}
 
 				for _, a := range currentAssignments {
-					fmt.Printf("%v: %v\n", strings.Title(a.Track), a.Slug)
+					if argc == 1 {
+						if strings.ToLower(language) == strings.ToLower(a.Track)  {
+							fmt.Printf("%v: %v\n", strings.Title(a.Track), a.Slug)
+						}
+					} else {
+						fmt.Printf("%v: %v\n", strings.Title(a.Track), a.Slug)
+					}
 				}
 			},
 		},
