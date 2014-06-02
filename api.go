90b1bd9674c2841e67fae14df07be29850586e7c
Katrina Owen <katrina.owen@gmail.com>
2014-06-02 15:44:12 -0600

    Bump version

--- api.go
+++ api.go
@@ -10,7 +10,7 @@ import (
 	"github.com/exercism/cli/configuration"
 )
 
-const VERSION = "1.6.1"
+const VERSION = "1.6.2"
 const USER_AGENT = "github.com/exercism/cli v" + VERSION
 
 var FetchEndpoints = map[string]string{
