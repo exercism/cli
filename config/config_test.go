51eac703006177aa78b83e457e0c8a6a3e8fd90d
Katrina Owen <katrina.owen@gmail.com>
2014-07-31 07:51:48 -0700

    Consolidate config package

--- config/config_test.go
+++ config/config_test.go
@@ -2,10 +2,32 @@ package config
 
 import (
 	"io/ioutil"
+	"os"
+	"path/filepath"
 	"testing"
 
 	"github.com/stretchr/testify/assert"
 )
+
+func TestDemoDir(t *testing.T) {
+	path, err := ioutil.TempDir("", "")
+	assert.NoError(t, err)
+	os.Chdir(path)
+
+	path, err = filepath.EvalSymlinks(path)
+	assert.NoError(t, err)
+
+	path = filepath.Join(path, "exercism-demo")
+
+	demoDir, err := demoDirectory()
+	assert.NoError(t, err)
+	assert.Equal(t, demoDir, path)
+}
+
+func TestExpandsTildeInExercismDirectory(t *testing.T) {
+	expandedDir := ReplaceTilde("~/exercism/directory")
+	assert.NotContains(t, "~", expandedDir)
+}
 
 func TestReadingWritingConfig(t *testing.T) {
 	tmpDir, err := ioutil.TempDir("", "")
--- config/replace_tilde.go
+++
@@ -1,9 +0,0 @@
-package config
-
-import (
-	"strings"
-)
-
-func ReplaceTilde(oldPath string) string {
-	return strings.Replace(oldPath, "~/", HomeDir()+"/", 1)
-}
