c90c621cbd5e72a45d00a657db93f9e82d1a2243
Katrina Owen <katrina.owen@gmail.com>
2014-07-31 08:05:06 -0700

    Add comments to config package

--- config/config.go
+++ config/config.go
@@ -11,11 +11,21 @@ import (
 )
 
 const (
-	File        = ".exercism.go"
-	Host        = "http://exercism.io"
+	// File is the default name of the JSON file where the config written.
+	// The user can pass an alternate filename when using the CLI.
+	// TODO: rename to .exercism.json
+	File = ".exercism.go"
+	// Host is the default hostname for fetching problems and submitting exercises.
+	// TODO: We need to operate against two hosts (one for problems and one for submissions),
+	// or define a proxy that both APIs can go through.
+	Host = "http://exercism.io"
+	// DemoDirname is the default directory to download problems to.
 	DemoDirname = "exercism-demo"
 )
 
+// Config represents the settings for particular user.
+// This defines both the auth for talking to the API, as well as
+// where to put problems that get downloaded.
 type Config struct {
 	GithubUsername    string `json:"githubUsername"`
 	APIKey            string `json:"apiKey"`
@@ -23,6 +33,7 @@ type Config struct {
 	Hostname          string `json:"hostname"`
 }
 
+// ToFile writes a Config to a JSON file.
 func ToFile(path string, c Config) error {
 	bytes, err := json.Marshal(c)
 	if err != nil {
@@ -37,10 +48,18 @@ func ToFile(path string, c Config) error {
 	return nil
 }
 
-func ReplaceTilde(oldPath string) string {
-	return strings.Replace(oldPath, "~/", HomeDir()+"/", 1)
+// FromFile loads a Config object from a JSON file.
+func FromFile(path string) (c Config, err error) {
+	bytes, err := ioutil.ReadFile(path)
+	if err != nil {
+		return
+	}
+
+	err = json.Unmarshal(bytes, &c)
+	return
 }
 
+// HomeDir return's the user's canonical home directory.
 // See: http://stackoverflow.com/questions/7922270/obtain-users-home-directory
 // we can't cross compile using cgo and use user.Current()
 func HomeDir() string {
@@ -54,20 +73,13 @@ func HomeDir() string {
 
 	return os.Getenv("HOME")
 }
-func FromFile(path string) (c Config, err error) {
-	bytes, err := ioutil.ReadFile(path)
-	if err != nil {
-		return
-	}
 
-	err = json.Unmarshal(bytes, &c)
-	return
-}
-
+// Filename is the name of the JSON file containing the user's config.
 func Filename(dir string) string {
 	return fmt.Sprintf("%s/%s", dir, File)
 }
 
+// Demo is a default configuration for unauthenticated users.
 func Demo() (c Config, err error) {
 	demoDir, err := demoDirectory()
 	if err != nil {
@@ -79,6 +91,11 @@ func Demo() (c Config, err error) {
 		ExercismDirectory: demoDir,
 	}
 	return
+}
+
+// ReplaceTilde replaces the short-hand home path with the absolute path.
+func ReplaceTilde(oldPath string) string {
+	return strings.Replace(oldPath, "~/", HomeDir()+"/", 1)
 }
 
 func demoDirectory() (dir string, err error) {
