9b65261fa13997787095633e45565f4d1dd0bf79
Katrina Owen <katrina.owen@gmail.com>
2014-08-02 22:06:52 -0700

    Stop pretending the code is organized

--- main.go
+++ main.go
@@ -1,8 +1,12 @@
 package main
 
 import (
+	"bufio"
+	"bytes"
+	"encoding/json"
 	"fmt"
 	"io/ioutil"
+	"net/http"
 	"os"
 	"path/filepath"
 	"strings"
@@ -10,6 +14,35 @@ import (
 	"github.com/codegangsta/cli"
 	"github.com/exercism/cli/config"
 )
+
+const (
+	// Version is the current release of the command-line app.
+	// We try to follow Semantic Versioning (http://semver.org),
+	// but with the http://exercism.io app being a prototype, a
+	// lot of things get out of hand.
+	Version = "1.6.2"
+	// UserAgent is sent along as a header to HTTP requests that the
+	// CLI makes. This helps with debugging.
+	UserAgent = "github.com/exercism/cli v" + Version
+)
+
+var FetchEndpoints = map[string]string{
+	"current":  "/api/v1/user/assignments/current",
+	"next":     "/api/v1/user/assignments/next",
+	"restore":  "/api/v1/user/assignments/restore",
+	"demo":     "/api/v1/assignments/demo",
+	"exercise": "/api/v1/assignments",
+}
+
+var testExtensions = map[string]string{
+	"ruby":    "_test.rb",
+	"js":      ".spec.js",
+	"elixir":  "_test.exs",
+	"clojure": "_test.clj",
+	"python":  "_test.py",
+	"go":      "_test.go",
+	"haskell": "_test.hs",
+}
 
 func main() {
 	app := cli.NewApp()
@@ -322,4 +355,294 @@ func main() {
 		fmt.Errorf("%v", err)
 		os.Exit(1)
 	}
+}
+
+func login(path string) (c config.Config, err error) {
+	c, err = askForConfigInfo()
+	if err != nil {
+		return
+	}
+	config.ToFile(path, c)
+	fmt.Printf("Your exercism directory can be found at %s\n", c.ExercismDirectory)
+	return
+}
+
+func logout(path string) {
+	os.Remove(path)
+}
+
+func absolutePath(path string) (string, error) {
+	path, err := filepath.Abs(path)
+	if err != nil {
+		return "", err
+	}
+	return filepath.EvalSymlinks(path)
+}
+
+func askForConfigInfo() (c config.Config, err error) {
+	var un, key, dir string
+	delim := "\r\n"
+
+	bio := bufio.NewReader(os.Stdin)
+
+	currentDir, err := os.Getwd()
+	if err != nil {
+		return
+	}
+
+	fmt.Print("Your GitHub username: ")
+	un, err = bio.ReadString('\n')
+	if err != nil {
+		return
+	}
+
+	fmt.Print("Your Exercism API key (found at http://exercism.io/account): ")
+	key, err = bio.ReadString('\n')
+	if err != nil {
+		return
+	}
+
+	fmt.Println("What is your exercism exercises project path?")
+	fmt.Printf("Press Enter to select the default (%s):\n", currentDir)
+	fmt.Print("> ")
+	dir, err = bio.ReadString('\n')
+	if err != nil {
+		return
+	}
+
+	key = strings.TrimRight(key, delim)
+	un = strings.TrimRight(un, delim)
+	dir = strings.TrimRight(dir, delim)
+
+	if dir == "" {
+		dir = currentDir
+	}
+
+	dir = config.ReplaceTilde(dir)
+
+	err = os.MkdirAll(dir, 0755)
+	if err != nil {
+		err = fmt.Errorf("Error making directory %v: [%v]", dir, err)
+		return
+	}
+
+	dir, err = absolutePath(dir)
+	if err != nil {
+		return
+	}
+
+	c = config.Config{GithubUsername: un, APIKey: key, ExercismDirectory: dir, Hostname: "http://exercism.io"}
+	return
+}
+
+type submitResponse struct {
+	ID             string `json:"id"`
+	Status         string `json:"status"`
+	Language       string `json:"language"`
+	Exercise       string `json:"exercise"`
+	SubmissionPath string `json:"submission_path"`
+	Error          string `json:"error"`
+}
+
+type submitRequest struct {
+	Key  string `json:"key"`
+	Code string `json:"code"`
+	Path string `json:"path"`
+}
+
+func FetchAssignments(c config.Config, path string) (as []Assignment, err error) {
+	url := fmt.Sprintf("%s%s?key=%s", c.Hostname, path, c.APIKey)
+	req, err := http.NewRequest("GET", url, nil)
+	if err != nil {
+		return
+	}
+
+	resp, err := http.DefaultClient.Do(req)
+	if err != nil {
+		err = fmt.Errorf("Error fetching assignments: [%v]", err)
+		return
+	}
+
+	body, err := ioutil.ReadAll(resp.Body)
+	resp.Body.Close()
+
+	if err != nil {
+		err = fmt.Errorf("Error fetching assignments: [%v]", err)
+		return
+	}
+
+	if resp.StatusCode != http.StatusOK {
+		var apiError struct {
+			Error string `json:"error"`
+		}
+		err = json.Unmarshal(body, &apiError)
+		if err != nil {
+			err = fmt.Errorf("Error parsing API response: [%v]", err)
+			return
+		}
+
+		err = fmt.Errorf("Error fetching assignments. HTTP Status Code: %d\n%s", resp.StatusCode, apiError.Error)
+		return
+	}
+
+	var fr struct {
+		Assignments []Assignment
+	}
+
+	err = json.Unmarshal(body, &fr)
+	if err != nil {
+		err = fmt.Errorf("Error parsing API response: [%v]", err)
+		return
+	}
+
+	return fr.Assignments, err
+}
+
+func UnsubmitAssignment(c config.Config) (r string, err error) {
+	path := "api/v1/user/assignments"
+
+	url := fmt.Sprintf("%s/%s?key=%s", c.Hostname, path, c.APIKey)
+
+	req, err := http.NewRequest("DELETE", url, nil)
+	if err != nil {
+		return
+	}
+
+	req.Header.Set("User-Agent", UserAgent)
+
+	resp, err := http.DefaultClient.Do(req)
+	if err != nil {
+		err = fmt.Errorf("Error destroying submission: [%v]", err)
+		return
+	}
+
+	body, err := ioutil.ReadAll(resp.Body)
+	resp.Body.Close()
+	if err != nil {
+		return
+	}
+
+	if resp.StatusCode != http.StatusNoContent {
+
+		var ur struct {
+			Error string
+		}
+
+		err = json.Unmarshal(body, &ur)
+		if err != nil {
+			return
+		}
+
+		err = fmt.Errorf("Status: %d, Error: %v", resp.StatusCode, ur.Error)
+		return ur.Error, err
+	}
+
+	return
+}
+func SubmitAssignment(c config.Config, filePath string, code []byte) (r submitResponse, err error) {
+	path := "api/v1/user/assignments"
+
+	url := fmt.Sprintf("%s/%s", c.Hostname, path)
+
+	submission := submitRequest{Key: c.APIKey, Code: string(code), Path: filePath}
+	submissionJSON, err := json.Marshal(submission)
+	if err != nil {
+		return
+	}
+
+	req, err := http.NewRequest("POST", url, bytes.NewReader(submissionJSON))
+	if err != nil {
+		return
+	}
+
+	req.Header.Set("User-Agent", UserAgent)
+	req.Header.Set("Content-Type", "application/json")
+
+	resp, err := http.DefaultClient.Do(req)
+	if err != nil {
+		err = fmt.Errorf("Error posting assignment: [%v]", err)
+		return
+	}
+
+	body, err := ioutil.ReadAll(resp.Body)
+	resp.Body.Close()
+	if err != nil {
+		return
+	}
+
+	if resp.StatusCode != http.StatusCreated {
+		err = json.Unmarshal(body, &r)
+		if err != nil {
+			return
+		}
+		err = fmt.Errorf("Status: %d, Error: %v", resp.StatusCode, r)
+		return
+	}
+
+	err = json.Unmarshal(body, &r)
+	if err != nil {
+		err = fmt.Errorf("Error parsing API response: [%v]", err)
+	}
+
+	return
+}
+
+type Assignment struct {
+	Track   string
+	Slug    string
+	Files   map[string]string
+	IsFresh bool `json:"fresh"`
+}
+
+func SaveAssignment(dir string, a Assignment) (err error) {
+	root := fmt.Sprintf("%s/%s/%s", dir, a.Track, a.Slug)
+
+	for name, text := range a.Files {
+		file := fmt.Sprintf("%s/%s", root, name)
+		dir := filepath.Dir(file)
+		err = os.MkdirAll(dir, 0755)
+		if err != nil {
+			err = fmt.Errorf("Error making directory %v: [%v]", dir, err)
+			return
+		}
+		if _, err = os.Stat(file); err != nil {
+			if os.IsNotExist(err) {
+				err = ioutil.WriteFile(file, []byte(text), 0644)
+				if err != nil {
+					err = fmt.Errorf("Error writing file %v: [%v]", name, err)
+					return
+				}
+			}
+		}
+	}
+
+	fresh := " "
+	if a.IsFresh {
+		fresh = "*"
+	}
+	fmt.Println(fresh, a.Track, "-", a.Slug)
+
+	return
+}
+
+func FetchEndpoint(args []string) string {
+	if len(args) == 0 {
+		return FetchEndpoints["current"]
+	}
+
+	endpoint := FetchEndpoints["exercise"]
+	for _, arg := range args {
+		endpoint = fmt.Sprintf("%s/%s", endpoint, arg)
+	}
+
+	return endpoint
+}
+
+func IsTest(filename string) bool {
+	for _, ext := range testExtensions {
+		if strings.LastIndex(filename, ext) > 0 {
+			return true
+		}
+	}
+	return false
 }
