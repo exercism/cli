81c38cdda08dd65df434eb9fc8ba9dcb0b4f192b
Luke Cowell <lcowell@gmail.com>
2014-08-11 13:19:39 -0700

    if the current config file end in .go, rename it to .json

--- config/config_test.go
+++ config/config_test.go
@@ -81,3 +81,83 @@ func TestEncodingConfig(t *testing.T) {
 	assert.NoError(t, err)
 	assert.Equal(t, sanitizedJSON, buf.String())
 }
+
+func TestNormalizeGoPresent(t *testing.T) {
+	withPreparedConfigDir(t, false, true, func(confDir, jsonPath, goPath string) {
+		err := NormalizeConfig(confDir)
+		assert.NoError(t, err)
+
+		assertFileExists(t, jsonPath)
+		assertNoFileExists(t, goPath)
+	})
+}
+
+func TestNormalizeJsonPresent(t *testing.T) {
+	withPreparedConfigDir(t, true, false, func(confDir, jsonPath, goPath string) {
+		err := NormalizeConfig(confDir)
+		assert.NoError(t, err)
+
+		assertFileExists(t, jsonPath)
+		assertNoFileExists(t, goPath)
+	})
+}
+
+func TestNormalizeBothPresent(t *testing.T) {
+	withPreparedConfigDir(t, true, true, func(confDir, jsonPath, goPath string) {
+		err := NormalizeConfig(confDir)
+		assert.NoError(t, err)
+
+		assertFileExists(t, jsonPath)
+		assertFileExists(t, goPath)
+	})
+}
+
+func TestNormalizeNeitherPresent(t *testing.T) {
+	withPreparedConfigDir(t, false, false, func(confDir, jsonPath, goPath string) {
+		err := NormalizeConfig(confDir)
+		assert.NoError(t, err)
+
+		assertNoFileExists(t, jsonPath)
+		assertNoFileExists(t, goPath)
+	})
+}
+
+func withPreparedConfigDir(t *testing.T, jsonExists, goExists bool, fn func(configPath, goPath, jsonPath string)) {
+	tmpDir, err := ioutil.TempDir("", "")
+	assert.NoError(t, err)
+
+	jsonPath := filepath.Join(tmpDir, ".exercism.json")
+	goPath := filepath.Join(tmpDir, ".exercism.go")
+
+	if jsonExists {
+		f, err := os.Create(jsonPath)
+		assert.NoError(t, err)
+		f.Close()
+	}
+	if goExists {
+		f, err := os.Create(goPath)
+		assert.NoError(t, err)
+		f.Close()
+	}
+
+	fn(tmpDir, jsonPath, goPath)
+
+	os.Remove(tmpDir)
+}
+
+func fileExists(path string) bool {
+	_, err := os.Stat(path)
+	return !os.IsNotExist(err)
+}
+
+func assertFileExists(t *testing.T, path string) {
+	if !fileExists(path) {
+		t.Error("expected", path, "to exist")
+	}
+}
+
+func assertNoFileExists(t *testing.T, path string) {
+	if fileExists(path) {
+		t.Error("expected", path, "to exist")
+	}
+}
