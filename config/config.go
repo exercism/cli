b515db09e1130e2a9823f1e28e806c0dfe8a70b1
Luke Cowell <lcowell@gmail.com>
2014-08-15 15:49:40 -0700

    panic if we are unable to determine where home is
    Returning a blank string if the user has an incorrectly setup
    environment could mean we puts things in unexpected places.

--- config/config.go
+++ config/config.go
@@ -108,15 +108,24 @@ func WithDefaultPath(p string) string {
 // See: http://stackoverflow.com/questions/7922270/obtain-users-home-directory
 // we can't cross compile using cgo and use user.Current()
 func HomeDir() string {
+	var home string
+
 	if runtime.GOOS == "windows" {
-		home := os.Getenv("HOMEDRIVE") + os.Getenv("HOMEPATH")
+		home = os.Getenv("HOMEDRIVE") + os.Getenv("HOMEPATH")
 		if home == "" {
 			home = os.Getenv("USERPROFILE")
 		}
-		return home
+	} else {
+		home = os.Getenv("HOME")
 	}
 
-	return os.Getenv("HOME")
+	// TODO should we fall back to the CWD instead ?
+	if home == "" {
+		panic("unable to determine the location of your home directory")
+	}
+
+	return home
+
 }
 
 // Filename is the name of the JSON file containing the user's config.
