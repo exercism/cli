5d58fd14b9db84fb752b3bf6112123cd6f04c532
Katrina Owen <katrina.owen@gmail.com>
2014-08-28 07:27:17 -0700

    Use home dir function from config
    user.Current() isn't implemented on all OS/architectures.

--- handlers/debug.go
+++ handlers/debug.go
@@ -3,7 +3,6 @@ package handlers
 import (
 	"fmt"
 	"log"
-	"os/user"
 	"runtime"
 
 	"github.com/codegangsta/cli"
@@ -11,29 +10,30 @@ import (
 )
 
 func Debug(ctx *cli.Context) {
-	usr, err := user.Current()
-	if err != nil {
-		log.Fatal(err)
+	bail := func(err error) {
+		if err != nil {
+			fmt.Printf("\nIf you are having any issues, please contact kytrinyx@exercism.io with this information.\n")
+			log.Fatal(err)
+		}
 	}
 
-	fmt.Printf("Debug Information\n")
+	fmt.Printf("\n**** Debug Information ****\n")
+	fmt.Printf("Exercism CLI Version: %s\n", ctx.App.Version)
 	fmt.Printf("OS/Architecture: %s/%s\n", runtime.GOOS, runtime.GOARCH)
-	fmt.Printf("Home Dir: %s\n", usr.HomeDir)
-	fmt.Printf("Version: %s\n", ctx.App.Version)
+
+	dir, err := config.Home()
+	bail(err)
+	fmt.Printf("Home Dir: %s\n", dir)
 
 	file, err := config.FilePath(ctx.GlobalString("config"))
-	if err != nil {
-		log.Fatal(err)
-	}
+	bail(err)
 
 	c, err := config.Read(file)
-	if err == nil {
-		fmt.Printf("\nExercism Configuration\n")
-		fmt.Printf("API Key: %s\n", c.APIKey)
-		fmt.Printf("Exercises Directory: %s\n", c.Dir)
-		fmt.Printf("Config file: %s\n", c.File())
-		fmt.Printf("API: %s\n", c.Hostname)
-	}
+	bail(err)
 
-	fmt.Printf("\nIf you are having any issues, please contact kytrinyx@exercism.io with this information.\n")
+	fmt.Printf("Config file: %s\n", c.File())
+	fmt.Printf("API: %s\n", c.Hostname)
+	fmt.Printf("API Key: %s\n", c.APIKey)
+	fmt.Printf("Exercises Directory: %s\n", c.Dir)
+	fmt.Println()
 }
