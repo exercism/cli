9149fe8495afb38a7fda74b8ee2862fe00101739
Katrina Owen <katrina.owen@gmail.com>
2014-08-29 06:55:55 -0700

    Delete 'current' command

--- main.go
+++ main.go
@@ -88,57 +88,6 @@ func main() {
 			Action: handlers.Configure,
 		},
 		{
-			Name:      "current",
-			ShortName: "c",
-			Usage:     "Show the current assignments",
-			Action: func(ctx *cli.Context) {
-				var language string
-				argc := len(ctx.Args())
-				if argc != 0 && argc != 1 {
-					fmt.Println("Usage: exercism current\n   or: exercism current LANGUAGE")
-					return
-				}
-
-				configPath := ctx.GlobalString("config")
-				err := normalizeConfigFile(configPath)
-				if err != nil {
-					fmt.Println(err)
-					return
-				}
-				c, err := config.Read(configPath)
-				if err != nil {
-					fmt.Println("Are you sure you are logged in? Please login again.")
-					c, err = login(configPath)
-					if err != nil {
-						fmt.Println(err)
-						return
-					}
-				}
-				currentAssignments, err := FetchAssignments(c, FetchEndpoints["current"])
-				if err != nil {
-					fmt.Println(err)
-					return
-				}
-
-				if argc == 1 {
-					language = ctx.Args()[0]
-					fmt.Println("Current Assignments for", strings.Title(language))
-				} else {
-					fmt.Println("Current Assignments")
-				}
-
-				for _, a := range currentAssignments {
-					if argc == 1 {
-						if strings.ToLower(language) == strings.ToLower(a.Track) {
-							fmt.Printf("%v: %v\n", strings.Title(a.Track), a.Slug)
-						}
-					} else {
-						fmt.Printf("%v: %v\n", strings.Title(a.Track), a.Slug)
-					}
-				}
-			},
-		},
-		{
 			Name:      "demo",
 			ShortName: "d",
 			Usage:     "Fetch first assignment for each language from exercism.io",
