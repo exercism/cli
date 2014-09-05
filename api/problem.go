0c12ea5730973e1ae20d9b95b2bd7e9d4eaac1f0
Katrina Owen <katrina.owen@gmail.com>
2014-09-04 21:13:52 -0700

    Actually save demo problems

--- api/problem.go
+++ api/problem.go
@@ -1,5 +1,12 @@
 package api
 
+import (
+	"fmt"
+	"io/ioutil"
+	"os"
+	"path/filepath"
+)
+
 type Problem struct {
 	ID       string            `json:"id"`
 	TrackID  string            `json:"track_id"`
@@ -10,6 +17,36 @@ type Problem struct {
 	Files    map[string]string `json:"files"`
 }
 
+func (p *Problem) String() {
+	fmt.Sprintf("%s - %s in %s", p.ID, p.Name, p.Language)
+}
+
+func (p *Problem) ExistsIn(dir string) bool {
+	path := fmt.Sprintf("%s/%s", dir, p.ID)
+
+	if _, err := os.Stat(path); err != nil && os.IsNotExist(err) {
+		return false
+	}
+	return true
+}
+
 func (p *Problem) Save(dir string) error {
+	if p.ExistsIn(dir) {
+		return nil
+	}
+
+	for name, text := range p.Files {
+		file := fmt.Sprintf("%s/%s/%s", dir, p.ID, name)
+
+		err := os.MkdirAll(filepath.Dir(file), 0755)
+		if err != nil {
+			return err
+		}
+
+		err = ioutil.WriteFile(file, []byte(text), 0644)
+		if err != nil {
+			return err
+		}
+	}
 	return nil
 }
