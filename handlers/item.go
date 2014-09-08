da5753f3890052e1da3f400e5cb5494c463113e2
Katrina Owen <katrina.owen@gmail.com>
2014-09-07 22:14:14 -0700

    Improve report when fetching problems

--- handlers/item.go
+++ handlers/item.go
@@ -11,14 +11,33 @@ import (
 
 type Item struct {
 	*api.Problem
-	dir string
+	dir       string
+	isNew     bool
+	isUpdated bool
 }
 
 func (it *Item) Path() string {
 	return fmt.Sprintf("%s/%s", it.dir, it.Problem.ID)
 }
 
+func (it *Item) Matches(filter HWFilter) bool {
+	switch filter {
+	case HWNew:
+		return it.isNew
+	case HWUpdated:
+		return it.isUpdated
+	}
+	return true
+}
+
 func (it *Item) Save() error {
+	if _, err := os.Stat(it.Path()); err != nil {
+		if !os.IsNotExist(err) {
+			return err
+		}
+		it.isNew = true
+	}
+
 	for name, text := range it.Files {
 		file := fmt.Sprintf("%s/%s/%s", it.dir, it.ID, name)
 
@@ -30,6 +49,10 @@ func (it *Item) Save() error {
 		if _, err := os.Stat(file); err != nil {
 			if !os.IsNotExist(err) {
 				return err
+			}
+
+			if !it.isNew {
+				it.isUpdated = true
 			}
 
 			err = ioutil.WriteFile(file, []byte(text), 0644)
