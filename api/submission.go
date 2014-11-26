4d455a72d18233ee074df3e2db808fc922abd538
fwslash <harry.harim.park@gmail.com>
2014-11-26 10:20:17 -0800

    Add download feature

--- api/submission.go
+++ api/submission.go
@@ -2,9 +2,12 @@ package api
 
 // Submission is an iteration that has been submitted to the API.
 type Submission struct {
-	URL      string `json:"url"`
-	TrackID  string `json:"track_id"`
-	Language string `json:"language"`
-	Slug     string `json:"slug"`
-	Name     string `json:"name"`
+	URL           string            `json:"url"`
+	TrackID       string            `json:"track_id"`
+	Language      string            `json:"language"`
+	Slug          string            `json:"slug"`
+	Name          string            `json:"name"`
+	Username      string            `json:"username"`
+	ProblemFiles  map[string]string `json:"problem_files"`
+	SolutionFiles map[string]string `json:"solution_files"`
 }
