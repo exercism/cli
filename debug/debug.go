5f34dd49286f8244458bfb437cdd5b5327c98164
Luke Cowell <lcowell@gmail.com>
2015-11-15 10:03:19 -0800

    add package for debugging output
    This package will print output to StdErr when the verbose flag is set for the cli.

---
+++ debug/debug.go
@@ -0,0 +1,25 @@
+package debug
+
+import (
+	"fmt"
+	"io"
+	"os"
+)
+
+var (
+	// Verbose determines if debugging output is displayed to the user
+	Verbose bool
+	output  io.Writer = os.Stderr
+)
+
+// Println conditionally outputs a message to Stderr
+func Println(args ...interface{}) {
+	Printf("%s\n", args...)
+}
+
+// Printf conditionally outputs a formatted message to Stderr
+func Printf(format string, args ...interface{}) {
+	if Verbose {
+		fmt.Fprintf(output, format, args...)
+	}
+}
