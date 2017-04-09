de5f5022a421de91a1b6e3e512c583395ebab5bc
Leo Correa <lcorr005@gmail.com>
2017-04-09 19:51:50 -0400

    Use go-update and semver for a better upgrade experience This should resolve https://github.com/exercism/cli/issues/381

--- cmd/release_utils.go
+++ cmd/release_utils.go
@@ -1,31 +1,11 @@
 package cmd
 
 import (
-	"archive/tar"
-	"archive/zip"
 	"bytes"
-	"compress/gzip"
-	"encoding/json"
 	"fmt"
-	"io"
 	"io/ioutil"
 	"net/http"
-	"os"
 	"strings"
-)
-
-var (
-	osMap = map[string]string{
-		"darwin":  "mac",
-		"linux":   "linux",
-		"windows": "windows",
-	}
-
-	archMap = map[string]string{
-		"amd64": "64bit",
-		"386":   "32bit",
-		"arm":   "arm",
-	}
 )
 
 type asset struct {
@@ -64,91 +44,4 @@ type release struct {
 
 func (r *release) Version() string {
 	return strings.TrimPrefix(r.TagName, "v")
-}
-
-const installFlag = os.O_RDWR | os.O_CREATE | os.O_TRUNC
-
-func installTgz(source *bytes.Reader, dest string) error {
-	gr, err := gzip.NewReader(source)
-	if err != nil {
-		return err
-	}
-	defer gr.Close()
-
-	tr := tar.NewReader(gr)
-	for {
-		hdr, err := tr.Next()
-		if err == io.EOF {
-			break
-		}
-		if err != nil {
-			return err
-		}
-
-		// Move the old version to a backup path that we can recover from
-		// in case the upgrade fails
-		destBackup := dest + ".bak"
-		if _, err := os.Stat(dest); err == nil {
-			os.Rename(dest, destBackup)
-		}
-
-		fileCopy, err := os.OpenFile(dest, installFlag, hdr.FileInfo().Mode())
-		if err != nil {
-			os.Rename(destBackup, dest)
-			return err
-		}
-		defer fileCopy.Close()
-
-		if _, err = io.Copy(fileCopy, tr); err != nil {
-			os.Rename(destBackup, dest)
-			return err
-		} else {
-			os.Remove(destBackup)
-		}
-	}
-
-	return nil
-}
-
-func installZip(source *bytes.Reader, dest string) error {
-	zr, err := zip.NewReader(source, int64(source.Len()))
-	if err != nil {
-		return err
-	}
-
-	for _, f := range zr.File {
-		fileCopy, err := os.OpenFile(dest, installFlag, f.Mode())
-		if err != nil {
-			return err
-		}
-		defer fileCopy.Close()
-
-		rc, err := f.Open()
-		if err != nil {
-			return err
-		}
-		defer rc.Close()
-
-		_, err = io.Copy(fileCopy, rc)
-		if err != nil {
-			return err
-		}
-	}
-
-	return nil
-}
-
-func fetchLatestRelease(client http.Client) (*release, error) {
-	resp, err := client.Get("https://api.github.com/repos/exercism/cli/releases/latest")
-	if err != nil {
-		return nil, err
-	}
-	defer resp.Body.Close()
-
-	var rel release
-	if err := json.NewDecoder(resp.Body).Decode(&rel); err != nil {
-		return nil, err
-	}
-
-	return &rel, nil
 }
