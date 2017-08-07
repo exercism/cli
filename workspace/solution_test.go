ddabd0abb859f1acd5d75e8734f14a06dd9ddc66
Katrina Owen <kytrinyx@github.com>
2017-08-07 17:44:00 -0600

    Add a string method to solution

--- workspace/solution_test.go
+++ workspace/solution_test.go
@@ -81,3 +81,62 @@ func TestSuffix(t *testing.T) {
 		assert.Equal(t, test.suffix, test.solution.Suffix())
 	}
 }
+
+func TestSolutionString(t *testing.T) {
+	tests := []struct {
+		solution Solution
+		desc     string
+	}{
+		{
+			solution: Solution{
+				Track:    "elixir",
+				Exercise: "secret-handshake",
+				Handle:   "",
+				Dir:      "",
+			},
+			desc: "elixir/secret-handshake",
+		},
+		{
+			solution: Solution{
+				Track:       "cpp",
+				Exercise:    "clock",
+				Handle:      "alice",
+				IsRequester: true,
+			},
+			desc: "cpp/clock",
+		},
+		{
+			solution: Solution{
+				Track:       "cpp",
+				Exercise:    "clock",
+				Handle:      "alice",
+				IsRequester: true,
+				Dir:         "/path/to/clock-2",
+			},
+			desc: "cpp/clock (2)",
+		},
+		{
+			solution: Solution{
+				Track:       "fsharp",
+				Exercise:    "hello-world",
+				Handle:      "bob",
+				IsRequester: false,
+			},
+			desc: "fsharp/hello-world by @bob",
+		},
+		{
+			solution: Solution{
+				Track:       "haskell",
+				Exercise:    "allergies",
+				Handle:      "charlie",
+				IsRequester: false,
+				Dir:         "/path/to/allergies-2",
+			},
+			desc: "haskell/allergies (2) by @charlie",
+		},
+	}
+
+	for _, test := range tests {
+		assert.Equal(t, test.desc, test.solution.String())
+	}
+}
