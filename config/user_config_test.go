e674a57a5c9f48da18017fbd68f1f9ca6f22cbb5
Rob Phoenix <rob@robphoenix.com>
2017-08-08 15:27:10 +0100

    [nextercism] Add user config tests for Windows

--- config/user_config_test.go
+++ config/user_config_test.go
@@ -1,8 +1,11 @@
+// +build !windows
+
 package config
 
 import (
 	"io/ioutil"
 	"os"
+	"path/filepath"
 	"testing"
 
 	"github.com/spf13/viper"
@@ -32,4 +35,28 @@ func TestUserConfig(t *testing.T) {
 	assert.NoError(t, err)
 	assert.Equal(t, "a", cfg.Token)
 	assert.Equal(t, "/a", cfg.Workspace)
+}
+
+func TestNormalizeWorkspace(t *testing.T) {
+	cwd, err := os.Getwd()
+	assert.NoError(t, err)
+
+	cfg := &UserConfig{Home: "/home/alice"}
+	tests := []struct {
+		in, out string
+	}{
+		{"", ""}, // don't make wild guesses
+		{"/home/alice///foobar", "/home/alice/foobar"},
+		{"~/foobar", "/home/alice/foobar"},
+		{"/foobar/~/noexpand", "/foobar/~/noexpand"},
+		{"/no/modification", "/no/modification"},
+		{"relative", filepath.Join(cwd, "relative")},
+		{"relative///path", filepath.Join(cwd, "relative", "path")},
+	}
+
+	for _, test := range tests {
+		cfg.Workspace = test.in
+		cfg.Normalize()
+		assert.Equal(t, test.out, cfg.Workspace)
+	}
 }
