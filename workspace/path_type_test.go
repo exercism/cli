8e8cce82c87fb48389edf31c62a1a6671d9381a4
Katrina Owen <kytrinyx@github.com>
2017-08-11 23:57:17 -0400

    Detect the type of a path
    We are pretty flexible in the type of argument that we receive. It might be
    an actual path, or it might be a conceptual path: the name of an exercise that
    we need to walk the workspace to find. If it's a conceptual path we might find
    more than one, in which case we'll do an interactive thing to disambiguate. If
    it's a real path, then we can do a lot less guessing.

---
+++ workspace/path_type_test.go
@@ -0,0 +1,62 @@
+package workspace
+
+import (
+	"path/filepath"
+	"runtime"
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+)
+
+func TestDetectPathType(t *testing.T) {
+	_, cwd, _, _ := runtime.Caller(0)
+	root := filepath.Join(cwd, "..", "..", "fixtures", "detect-path-type")
+
+	tests := []struct {
+		desc string
+		path string
+		pt   PathType
+	}{
+		{
+			desc: "absolute dir",
+			path: filepath.Join(root, "a-dir"),
+			pt:   TypeDir,
+		},
+		{
+			desc: "relative dir",
+			path: filepath.Join("..", "fixtures", "detect-path-type", "a-dir"),
+			pt:   TypeDir,
+		},
+		{
+			desc: "symlinked dir",
+			path: filepath.Join(root, "symlinked-dir"),
+			pt:   TypeDir,
+		},
+		{
+			desc: "absolute file",
+			path: filepath.Join(root, "a-file.txt"),
+			pt:   TypeFile,
+		},
+		{
+			desc: "relative file",
+			path: filepath.Join("..", "fixtures", "detect-path-type", "a-file.txt"),
+			pt:   TypeFile,
+		},
+		{
+			desc: "symlinked file",
+			path: filepath.Join(root, "symlinked-file.txt"),
+			pt:   TypeFile,
+		},
+		{
+			desc: "exercise ID",
+			path: "a-file",
+			pt:   TypeExerciseID,
+		},
+	}
+
+	for _, test := range tests {
+		pt, err := DetectPathType(test.path)
+		assert.NoError(t, err, test.desc)
+		assert.Equal(t, test.pt, pt, test.desc)
+	}
+}
