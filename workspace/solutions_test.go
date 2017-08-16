937df72339997b9353ce9f4a14d7b073463a08eb
Katrina Owen <kytrinyx@github.com>
2017-08-16 08:53:23 -0600

    Implement function to load up solutions in a slice of paths

--- workspace/solutions_test.go
+++ workspace/solutions_test.go
@@ -1,10 +1,39 @@
 package workspace
 
 import (
+	"path/filepath"
+	"runtime"
 	"testing"
 
 	"github.com/stretchr/testify/assert"
 )
+
+func TestNewSolutions(t *testing.T) {
+	_, cwd, _, _ := runtime.Caller(0)
+	root := filepath.Join(cwd, "..", "..", "fixtures", "solutions")
+
+	paths := []string{
+		filepath.Join(root, "alpha"),
+		filepath.Join(root, "bravo"),
+		filepath.Join(root, "charlie"),
+	}
+	sx, err := NewSolutions(paths)
+	assert.NoError(t, err)
+
+	if assert.Equal(t, 3, len(sx)) {
+		assert.Equal(t, "alpha", sx[0].ID)
+		assert.Equal(t, "bravo", sx[1].ID)
+		assert.Equal(t, "charlie", sx[2].ID)
+	}
+
+	paths = []string{
+		filepath.Join(root, "alpha"),
+		filepath.Join(root, "delta"),
+		filepath.Join(root, "bravo"),
+	}
+	_, err = NewSolutions(paths)
+	assert.Error(t, err)
+}
 
 func TestSolutions(t *testing.T) {
 	solutions := []*Solution{
