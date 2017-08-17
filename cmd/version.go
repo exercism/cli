111d9ddd9f6a7a62bf1e25547094622dfa78f175
Katrina Owen <kytrinyx@github.com>
2017-08-17 09:56:11 -0600

    Bump version to 3.0.0-alpha.2

--- cmd/version.go
+++ cmd/version.go
@@ -9,7 +9,7 @@ import (
 
 // Version is the version of the current build.
 // It follows semantic versioning.
-const Version = "3.0.0-alpha.1"
+const Version = "3.0.0-alpha.2"
 
 // checkLatest flag for version command.
 var checkLatest bool
