e00d3f89aab26a70aaeeb68c8e7311c4274551a6
Katrina Owen <kytrinyx@github.com>
2017-08-17 08:59:03 -0600

    Implement prepare command

--- cmd/prepare.go
+++ cmd/prepare.go
@@ -1,8 +1,12 @@
 package cmd
 
 import (
+	"errors"
 	"fmt"
+	"net/http"
 
+	"github.com/exercism/cli/api"
+	"github.com/exercism/cli/config"
 	"github.com/spf13/cobra"
 )
 
@@ -10,7 +14,7 @@ import (
 var prepareCmd = &cobra.Command{
 	Use:     "prepare",
 	Aliases: []string{"p"},
-	Short:   "Prepare does generic setup for Exercism and its tracks.",
+	Short:   "Prepare does setup for Exercism and its tracks.",
 	Long: `Prepare downloads settings and dependencies for Exercism and the language tracks.
 
 When called without any arguments, this downloads all the copy for the CLI so we
@@ -20,13 +24,73 @@ of the API endpoints to use.
 When called with a track ID, it will do specific setup for that track. This
 might include downloading the files that the track maintainers have said are
 necessary for the track in general. Any files that are only necessary for a specific
-exercise will only be downloaded with the exercise.
+exercise will be downloaded along with the exercise.
 
 To customize the CLI to suit your own preferences, use the configure command.
 	`,
 	Run: func(cmd *cobra.Command, args []string) {
-		fmt.Println("prepare called")
+		track, err := cmd.Flags().GetString("track")
+		BailOnError(err)
+
+		if track == "" {
+			fmt.Println("prepare called")
+			return
+		}
+		err = prepareTrack(track)
+		BailOnError(err)
 	},
+}
+
+func prepareTrack(id string) error {
+	apiCfg, err := config.NewAPIConfig()
+	if err != nil {
+		return err
+	}
+
+	client, err := api.NewClient()
+	if err != nil {
+		return err
+	}
+	url := fmt.Sprintf(apiCfg.URL("prepare-track"), id)
+
+	req, err := client.NewRequest("GET", url, nil)
+	if err != nil {
+		return err
+	}
+
+	payload := &prepareTrackPayload{}
+	res, err := client.Do(req, payload)
+	if err != nil {
+		return err
+	}
+
+	if res.StatusCode != http.StatusOK {
+		return errors.New("api call failed")
+	}
+
+	cliCfg, err := config.NewCLIConfig()
+	if err != nil {
+		return err
+	}
+
+	t, ok := cliCfg.Tracks[id]
+	if !ok {
+		t = config.NewTrack(id)
+	}
+	if payload.Track.TestPattern != "" {
+		t.IgnorePatterns = append(t.IgnorePatterns, payload.Track.TestPattern)
+	}
+	cliCfg.Tracks[id] = t
+
+	return cliCfg.Write()
+}
+
+type prepareTrackPayload struct {
+	Track struct {
+		ID          string `json:"id"`
+		Language    string `json:"language"`
+		TestPattern string `json:"test_pattern"`
+	} `json:"track"`
 }
 
 func initPrepareCmd() {
@@ -35,4 +99,5 @@ func initPrepareCmd() {
 
 func init() {
 	RootCmd.AddCommand(prepareCmd)
+	initPrepareCmd()
 }
