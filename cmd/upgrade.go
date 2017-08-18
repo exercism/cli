4dfece5286996873af33e149b07ea19272ce9d80
nywilken <dev@wilkenrivera.com>
2017-08-18 17:10:09 -0400

    Implement upgrade command
    Closes #414

--- cmd/upgrade.go
+++ cmd/upgrade.go
@@ -2,9 +2,15 @@ package cmd
 
 import (
 	"fmt"
+	"io"
+	"os"
 
+	"github.com/exercism/cli/cli"
+	"github.com/exercism/cli/debug"
 	"github.com/spf13/cobra"
 )
+
+var out io.Writer
 
 // upgradeCmd downloads and installs the most recent version of the CLI.
 var upgradeCmd = &cobra.Command{
@@ -13,18 +19,41 @@ var upgradeCmd = &cobra.Command{
 	Short:   "Upgrade to the latest version of the CLI.",
 	Long: `Upgrade to the latest version of the CLI.
 
-	This finds and downloads the latest release, if you don't
-	already have it.
+This finds and downloads the latest release, if you don't
+already have it.
 
-	On Windows the old CLI will be left on disk, marked as hidden.
-	The next time you upgrade, the hidden file will be overwritten.
-	You can always delete this file.
+On Windows the old CLI will be left on disk, marked as hidden.
+The next time you upgrade, the hidden file will be overwritten.
+You can always delete this file.
 	`,
 	Run: func(cmd *cobra.Command, args []string) {
-		fmt.Println("upgrade called")
+		if verbose, _ := cmd.Flags().GetBool("verbose"); verbose {
+			debug.Verbose = verbose
+		}
+
+		c := cli.New(Version)
+		err := updateCLI(c)
+		BailOnError(err)
 	},
+}
+
+// updateCLI updates CLI to the latest available version, if it is out of date.
+func updateCLI(c cli.Updater) error {
+	ok, err := c.IsUpToDate()
+	if err != nil {
+		return err
+	}
+
+	if ok {
+		fmt.Fprintln(out, "Your CLI version is up to date.")
+		return nil
+	}
+
+	return c.Upgrade()
 }
 
 func init() {
 	RootCmd.AddCommand(upgradeCmd)
+	upgradeCmd.Flags().BoolP("verbose", "v", false, "verbose output")
+	out = os.Stdout
 }
