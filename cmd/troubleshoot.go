f0fc66f8efc840d4ba1f5fd47c6a8ef3bcdcd174
Katrina Owen <kytrinyx@github.com>
2017-08-19 12:53:16 -0600

    Bring in troubleshoot command from robphoenix

--- cmd/troubleshoot.go
+++ cmd/troubleshoot.go
@@ -2,26 +2,43 @@ package cmd
 
 import (
 	"fmt"
+	"net/http"
+	"time"
 
+	"github.com/exercism/cli/cli"
+	"github.com/exercism/cli/config"
 	"github.com/spf13/cobra"
 )
+
+// fullAPIKey flag for troubleshoot command.
+var fullAPIKey bool
 
 // troubleshootCmd does a diagnostic self-check.
 var troubleshootCmd = &cobra.Command{
 	Use:     "troubleshoot",
 	Aliases: []string{"t"},
 	Short:   "Troubleshoot does a diagnostic self-check.",
-	Long: `Troubleshoot provides output to help with troubleshooting.
+	Long: `Provides output to help with troubleshooting.
 
-If you're running into trouble, then run the troubleshoot command
-and copy and paste the output into a GitHub issue so we can help
-figure out what's going on.
-	`,
+If you're running into trouble, copy and paste the output from the troubleshoot
+command into a GitHub issue so we can help figure out what's going on.
+`,
 	Run: func(cmd *cobra.Command, args []string) {
-		fmt.Println("troubleshoot called")
+		cli.HTTPClient = &http.Client{Timeout: 20 * time.Second}
+		c := cli.New(Version)
+
+		cfg, err := config.NewUserConfig()
+		BailOnError(err)
+
+		status := cli.NewStatus(c, *cfg)
+		status.Censor = !fullAPIKey
+		s, err := status.Check()
+		BailOnError(err)
+		fmt.Printf("%s", s)
 	},
 }
 
 func init() {
 	RootCmd.AddCommand(troubleshootCmd)
+	troubleshootCmd.Flags().BoolVarP(&fullAPIKey, "full-api-key", "f", false, "display the user's full API key, censored by default")
 }
