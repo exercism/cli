b7283ddfb74f5704b67efceb59cb6fa864b3f286
Katrina Owen <kytrinyx@github.com>
2017-08-20 11:47:29 -0600

    Fix command tests for Windows

--- cmd/configure_test.go
+++ cmd/configure_test.go
@@ -1,6 +1,8 @@
 package cmd
 
 import (
+	"path/filepath"
+	"runtime"
 	"testing"
 
 	"github.com/exercism/cli/config"
@@ -8,6 +10,17 @@ import (
 )
 
 func TestConfigure(t *testing.T) {
+
+	// Path stuff is complicated and platform-dependent.
+	var root string
+	if runtime.GOOS == "windows" {
+		cfg := config.NewEmptyUserConfig()
+		cfg.Normalize()
+		root = cfg.Home
+	} else {
+		root = "/"
+	}
+
 	tests := []struct {
 		desc           string
 		args           []string
@@ -20,7 +33,7 @@ func TestConfigure(t *testing.T) {
 			desc:           "It writes the flags when there is no config file.",
 			args:           []string{"fakeapp", "configure", "--token", "a", "--workspace", "/a", "--api", "http://example.com"},
 			existingUsrCfg: nil,
-			expectedUsrCfg: &config.UserConfig{Token: "a", Workspace: "/a"},
+			expectedUsrCfg: &config.UserConfig{Token: "a", Workspace: filepath.Join(root, "a")},
 			existingAPICfg: nil,
 			expectedAPICfg: &config.APIConfig{BaseURL: "http://example.com"},
 		},
@@ -28,7 +41,7 @@ func TestConfigure(t *testing.T) {
 			desc:           "It overwrites the flags in the config file.",
 			args:           []string{"fakeapp", "configure", "--token", "b", "--workspace", "/b", "--api", "http://example.com/v2"},
 			existingUsrCfg: &config.UserConfig{Token: "token-b", Workspace: "/workspace-b"},
-			expectedUsrCfg: &config.UserConfig{Token: "b", Workspace: "/b"},
+			expectedUsrCfg: &config.UserConfig{Token: "b", Workspace: filepath.Join(root, "b")},
 			existingAPICfg: &config.APIConfig{BaseURL: "http://example.com/v1"},
 			expectedAPICfg: &config.APIConfig{BaseURL: "http://example.com/v2"},
 		},
@@ -36,7 +49,7 @@ func TestConfigure(t *testing.T) {
 			desc:           "It overwrites the flags that are passed, without losing the ones that are not.",
 			args:           []string{"fakeapp", "configure", "--token", "c"},
 			existingUsrCfg: &config.UserConfig{Token: "token-c", Workspace: "/workspace-c"},
-			expectedUsrCfg: &config.UserConfig{Token: "c", Workspace: "/workspace-c"},
+			expectedUsrCfg: &config.UserConfig{Token: "c", Workspace: filepath.Join(root, "workspace-c")},
 		},
 		{
 			desc:           "It gets the default API base URL.",
@@ -60,6 +73,12 @@ func TestConfigure(t *testing.T) {
 			cfg := config.NewEmptyUserConfig()
 			cfg.Token = test.existingUsrCfg.Token
 			cfg.Workspace = test.existingUsrCfg.Workspace
+			err := cfg.Write()
+			assert.NoError(t, err, test.desc)
+		}
+		if test.existingAPICfg != nil {
+			// Write a fake config.
+			cfg := config.NewEmptyAPIConfig()
 			err := cfg.Write()
 			assert.NoError(t, err, test.desc)
 		}
