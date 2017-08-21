229fe6d8d619a04e306d09e9de8c6e1372f73888
Katrina Owen <kytrinyx@github.com>
2017-08-21 14:01:32 -0600

    Move interactive stuff into a new comms package

--- workspace/solutions_test.go
+++ workspace/solutions_test.go
@@ -34,32 +34,3 @@ func TestNewSolutions(t *testing.T) {
 	_, err = NewSolutions(paths)
 	assert.Error(t, err)
 }
-
-func TestSolutions(t *testing.T) {
-	solutions := []*Solution{
-		{Track: "a", Exercise: "foo"},
-		{Track: "b", Exercise: "foo"},
-		{Track: "c", Exercise: "foo"},
-	}
-	sx := Solutions(solutions)
-	display := "  [1] a/foo\n  [2] b/foo\n  [3] c/foo\n"
-	assert.Equal(t, display, sx.Display())
-
-	_, err := sx.Get(0)
-	assert.Error(t, err)
-
-	a, err := sx.Get(1)
-	assert.NoError(t, err)
-	assert.Equal(t, "a", a.Track)
-
-	b, err := sx.Get(2)
-	assert.NoError(t, err)
-	assert.Equal(t, "b", b.Track)
-
-	c, err := sx.Get(3)
-	assert.NoError(t, err)
-	assert.Equal(t, "c", c.Track)
-
-	_, err = sx.Get(4)
-	assert.Error(t, err)
-}
