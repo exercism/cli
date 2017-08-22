a97dba025e779cc9d00a24df7f0d50c3f0533629
Katrina Owen <kytrinyx@github.com>
2017-08-22 14:51:42 -0600

    Create small abstraction for interactive -uh- interactions
    If someone tries downloading or submitting something before
    they've configured their token or workspace, we're going to want
    to ask them to paste it to us.
    
    For the workspace we want to be able to provide a default suggestion.
    
    This Question abstraction provides a very small wrapper around such
    an interaction, and makes it testable by providing the Reader/Writer
    which can be overwritten with pre-populated readers, or a writer that
    discards everything.

---
+++ comms/question_test.go
@@ -0,0 +1,33 @@
+package comms
+
+import (
+	"io/ioutil"
+	"strings"
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+)
+
+func TestQuestion(t *testing.T) {
+	tests := []struct {
+		desc     string
+		given    string
+		fallback string
+		expected string
+	}{
+		{"records interactive response", "hello\n", "", "hello"},
+		{"responds with default if response is empty", "\n", "Fine.", "Fine."},
+	}
+	for _, test := range tests {
+		q := &Question{
+			Reader:       strings.NewReader(test.given),
+			Writer:       ioutil.Discard,
+			Prompt:       "Say something: ",
+			DefaultValue: test.fallback,
+		}
+
+		answer, err := q.Ask()
+		assert.NoError(t, err)
+		assert.Equal(t, answer, test.expected, test.desc)
+	}
+}
