9eed5a381ce156c7e8e85339cdf970c3c81fa5db
Fabien Tregan <FabienTregan@users.noreply.github.com>
2018-06-17 12:50:37 +0200

    Breaks out TestQuestion into subtests.

--- comms/question_test.go
+++ comms/question_test.go
@@ -9,7 +9,7 @@ import (
 )
 
 func TestQuestion(t *testing.T) {
-	tests := []struct {
+	testCases := []struct {
 		desc     string
 		given    string
 		fallback string
@@ -18,16 +18,18 @@ func TestQuestion(t *testing.T) {
 		{"records interactive response", "hello\n", "", "hello"},
 		{"responds with default if response is empty", "\n", "Fine.", "Fine."},
 	}
-	for _, test := range tests {
-		q := &Question{
-			Reader:       strings.NewReader(test.given),
-			Writer:       ioutil.Discard,
-			Prompt:       "Say something: ",
-			DefaultValue: test.fallback,
-		}
+	for _, tc := range testCases {
+		t.Run(tc.desc, func(t *testing.T) {
+			q := &Question{
+				Reader:       strings.NewReader(tc.given),
+				Writer:       ioutil.Discard,
+				Prompt:       "Say something: ",
+				DefaultValue: tc.fallback,
+			}
 
-		answer, err := q.Ask()
-		assert.NoError(t, err)
-		assert.Equal(t, answer, test.expected, test.desc)
+			answer, err := q.Ask()
+			assert.NoError(t, err)
+			assert.Equal(t, answer, tc.expected)
+		})
 	}
 }
