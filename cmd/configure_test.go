5df49d506aa5fb672da0d8b591e131c383ec285e
Katrina Owen <kytrinyx@github.com>
2018-07-07 08:35:42 +0100

    Tweak test values in config command test for clarity
    Use test values that help clarify that each test case is independent of each other test case,
    and also use descriptive values that help provide better symmetry (new vs old, original vs replacement,
    unmodified).

--- cmd/configure_test.go
+++ cmd/configure_test.go
@@ -18,28 +18,41 @@ type testCase struct {
 func TestConfigure(t *testing.T) {
 	testCases := []testCase{
 		testCase{
-			desc:           "It writes the flags when there is no config file.",
-			args:           []string{"fakeapp", "configure", "--token", "a", "--workspace", "/a", "--api", "http://example.com", "--skip-auth"},
+			desc: "It writes the flags when there is no config file.",
+			args: []string{
+				"fakeapp", "configure", "--skip-auth",
+				"--token", "abc123",
+				"--workspace", "/workspace",
+				"--api", "http://api.example.com",
+			},
 			existingUsrCfg: nil,
-			expectedUsrCfg: &config.UserConfig{Token: "a", Workspace: "/a", APIBaseURL: "http://example.com"},
+			expectedUsrCfg: &config.UserConfig{Token: "abc123", Workspace: "/workspace", APIBaseURL: "http://api.example.com"},
 		},
 		testCase{
-			desc:           "It overwrites the flags in the config file.",
-			args:           []string{"fakeapp", "configure", "--token", "b", "--workspace", "/b", "--api", "http://example.com/v2", "--skip-auth"},
-			existingUsrCfg: &config.UserConfig{Token: "token-b", Workspace: "/workspace-b", APIBaseURL: "http://example.com/v1"},
-			expectedUsrCfg: &config.UserConfig{Token: "b", Workspace: "/b", APIBaseURL: "http://example.com/v2"},
+			desc: "It overwrites the flags in the config file.",
+			args: []string{
+				"fakeapp", "configure", "--skip-auth",
+				"--token", "new-token",
+				"--workspace", "/new-workspace",
+				"--api", "http://new.example.com",
+			},
+			existingUsrCfg: &config.UserConfig{Token: "old-token", Workspace: "/old-workspace", APIBaseURL: "http://old.example.com"},
+			expectedUsrCfg: &config.UserConfig{Token: "new-token", Workspace: "/new-workspace", APIBaseURL: "http://new.example.com"},
 		},
 		testCase{
-			desc:           "It overwrites the flags that are passed, without losing the ones that are not.",
-			args:           []string{"fakeapp", "configure", "--token", "c", "--skip-auth"},
-			existingUsrCfg: &config.UserConfig{Token: "token-c", Workspace: "/workspace-c", APIBaseURL: "http://example.com"},
-			expectedUsrCfg: &config.UserConfig{Token: "c", Workspace: "/workspace-c", APIBaseURL: "http://example.com"},
+			desc: "It overwrites the flags that are passed, without losing the ones that are not.",
+			args: []string{
+				"fakeapp", "configure", "--skip-auth",
+				"--token", "replacement-token",
+			},
+			existingUsrCfg: &config.UserConfig{Token: "original-token", Workspace: "/unmodified", APIBaseURL: "http://unmodified.example.com"},
+			expectedUsrCfg: &config.UserConfig{Token: "replacement-token", Workspace: "/unmodified", APIBaseURL: "http://unmodified.example.com"},
 		},
 		testCase{
 			desc:           "It gets the default API base url.",
 			args:           []string{"fakeapp", "configure", "--skip-auth"},
-			existingUsrCfg: &config.UserConfig{Workspace: "/workspace-c"},
-			expectedUsrCfg: &config.UserConfig{Workspace: "/workspace-c", APIBaseURL: "https://v2.exercism.io/api/v1"},
+			existingUsrCfg: &config.UserConfig{Workspace: "/configured-workspace"},
+			expectedUsrCfg: &config.UserConfig{Workspace: "/configured-workspace", APIBaseURL: "https://v2.exercism.io/api/v1"},
 		},
 	}
 
