4dbd666ed34bf7c24680fd5cd1e84d0961982385
Katrina Owen <kytrinyx@github.com>
2018-07-08 13:42:46 +0100

    Add a newline to the printed error before exiting

--- cmd/root.go
+++ cmd/root.go
@@ -45,7 +45,7 @@ Download exercises and submit your solutions.`,
 // Execute adds all child commands to the root command.
 func Execute() {
 	if err := RootCmd.Execute(); err != nil {
-		fmt.Fprintf(Err, err.Error())
+		fmt.Fprintf(Err, "%s\n", err.Error())
 		os.Exit(-1)
 	}
 }
