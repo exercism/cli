4d6f29243b61ff494e8cbd21241d7954b650ed35
Katrina Owen <kytrinyx@github.com>
2018-07-14 10:33:39 +0100

    Move duplicated welcome message to constant

---
+++ cmd/cmd.go
@@ -0,0 +1,16 @@
+package cmd
+
+const msgWelcomePleaseConfigure = `
+
+    Welcome to Exercism!
+
+    To get started, you need to configure the the tool with your API token.
+    Find your token at
+
+        %s
+
+    Then run the configure command:
+
+        %s configure --token=YOUR_TOKEN
+
+`
--- cmd/download.go
+++ cmd/download.go
@@ -36,22 +36,7 @@ Download other people's solutions by providing the UUID.
 		}
 		if usrCfg.Token == "" {
 			tokenURL := config.InferSiteURL(usrCfg.APIBaseURL) + "/my/settings"
-			msg := `
-
-    Welcome to Exercism!
-
-    To get started, you need to configure the the tool with your API token.
-    Find your token at
-
-        %s
-
-    Then run the configure command:
-
-
-        %s configure --token=YOUR_TOKEN
-
-    `
-			return fmt.Errorf(msg, tokenURL, BinaryName)
+			return fmt.Errorf(msgWelcomePleaseConfigure, tokenURL, BinaryName)
 		}
 
 		uuid, err := cmd.Flags().GetString("uuid")
