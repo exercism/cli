77981f889c0d38e0cf5651da6708ecfb78422d5e
Katrin Leinweber <9948149+katrinleinweber@users.noreply.github.com>
2018-10-06 22:04:15 +0200

    Attempt to remove no-longer-supported commands

--- shell/exercism_completion.zsh
+++ shell/exercism_completion.zsh
@@ -5,12 +5,7 @@ _exercism() {
     local -a options
     options=(debug:"Outputs useful debug information."
              configure:"Writes config values to a JSON file."
-             demo:"Fetches a demo problem for each language track on exercism.io."
-             fetch:"Fetches your current problems on exercism.io, as well as the next unstarted problem in each language."
-             restore:"Restores completed and current problems on from exercism.iolong with your most recent iteration for each."
              submit:"Submits a new iteration to a problem on exercism.io."
-             unsubmit:"Deletes the most recently submitted iteration."
-             tracks:"List the available language tracks"
              download:"Downloads and saves a specified submission into the local system"
              help:"Shows a list of commands or help for one command")
 
