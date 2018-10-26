1b92fcd00cd58a6bbcf8f940bb0fa13d2d95cd64
antonycs <antonycs007@gmail.com>
2018-10-26 18:04:23 +0530

    Fixed issue #552 < Enhance workspace command to include --exercise, --track, and --team >

--- cmd/workspace.go
+++ cmd/workspace.go
@@ -6,8 +6,17 @@ import (
 	"github.com/exercism/cli/config"
 	"github.com/spf13/cobra"
 	"github.com/spf13/viper"
+	"os"
+	"path/filepath"
+	"sort"
+	"strings"
 )
 
+var checkTrack, checkExercise bool
+var treeLevel int = 1
+type caseInsensitive struct {
+	values []string
+}
 // workspaceCmd outputs the path to the person's workspace directory.
 var workspaceCmd = &cobra.Command{
 	Use:     "workspace",
@@ -36,11 +45,90 @@ nothing will happen.
 		// Ignore error. If the file doesn't exist, that is fine.
 		_ = v.ReadInConfig()
 
-		fmt.Fprintf(Out, "%s\n", v.GetString("workspace"))
+		
+
+		var workspaceDir = v.GetString("workspace")
+		if checkTrack {
+			treeLevel = 1
+			treePrint(workspaceDir)
+		} else if checkExercise {
+			treeLevel = 2
+			treePrint(workspaceDir)
+		} else {
+			fmt.Fprintf(Out, "%s\n", workspaceDir)
+		}
 		return nil
 	},
 }
 
+func treePrint(path string) (int) {
+	path, err := filepath.Abs(path)
+	if err != nil {
+		return 0
+	}
+	_ = nodeVisit(path, "", 0)
+	return 0
+}
+
+func nodeVisit(path, indent string, levelCheck int) (int) {
+	fi, err := os.Stat(path)
+	if err != nil {
+		return 0
+	}
+	fmt.Fprintf(Out, "%s\n", fi.Name())
+	if !fi.IsDir() {
+		return 0
+	}
+	dir, err := os.Open(path)
+	if err != nil {
+		return 0
+	}
+	names, err := dir.Readdirnames(-1)
+	_ = dir.Close() // safe to ignore this error.
+	if err != nil {
+		return 0
+	}
+	names = removeHidden(names)
+	sort.Sort(caseInsensitive{names})
+	if levelCheck == treeLevel{
+		return 1
+	}
+	add := "│   "
+	for i, name := range names {
+		if i == len(names)-1 {
+			fmt.Fprintf(Out, indent + "└── ")
+			add = "    "
+		} else {
+			fmt.Fprintf(Out, indent + "├── ")
+		}
+		nodeVisit(filepath.Join(path, name), indent+add, levelCheck+1)
+	}
+	return 0
+}
+
+func removeHidden(files []string) []string {
+	var clean []string
+	for _, f := range files {
+		if f[0] != '.' && !strings.Contains(f, ".go") && f != ".exercism" && !strings.Contains(f, ".json") {
+			clean = append(clean, f)
+		}
+	}
+	return clean
+}
+func (ci caseInsensitive) Len() int {
+	return len(ci.values)
+}
+
+func (ci caseInsensitive) Less(i, j int) bool {
+	return strings.ToLower(ci.values[i]) < strings.ToLower(ci.values[j])
+}
+
+func (ci caseInsensitive) Swap(i, j int) {
+	ci.values[i], ci.values[j] = ci.values[j], ci.values[i]
+}
+
 func init() {
 	RootCmd.AddCommand(workspaceCmd)
+	workspaceCmd.Flags().BoolVarP(&checkTrack, "track", "t", false, "Shows current track")
+	workspaceCmd.Flags().BoolVarP(&checkExercise, "exercise", "e", false, "Shows current exercise")
 }
