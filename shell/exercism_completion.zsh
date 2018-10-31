e98310fb09c85e1813d8078a4af6e162f6911025
nywilken <dev@wilkenrivera.com>
2018-10-30 21:32:04 -0400

    Update shell tab completion for exercism cli
    resolves #666

--- shell/exercism_completion.zsh
+++ shell/exercism_completion.zsh
@@ -3,17 +3,20 @@ _exercism() {
     typeset -A opt_args
 
     local -a options
-    options=(debug:"Outputs useful debug information."
-             configure:"Writes config values to a JSON file."
-             submit:"Submits a new iteration to a problem on exercism.io."
+    options=(configure:"Writes config values to a JSON file."
              download:"Downloads and saves a specified submission into the local system"
+             open:"Opens a browser to exercism.io for the specified submission."
+             submit:"Submits a new iteration to a problem on exercism.io."
+             troubleshoot:"Outputs useful debug information."
+             upgrade:"Upgrades to the latest available version."
+             version:"Outputs version information."
+             workspace:"Outputs the root directory for Exercism exercises."
              help:"Shows a list of commands or help for one command")
 
     _arguments -s -S \
-        {-c,--config}"[path to config file]:file:_files"    \
-        {-d,--debug}"[turn on verbose logging]" \
         {-h,--help}"[show help]"                \
-        {-v,--version}"[print the version]"     \
+        {-t,--timeout}"[override default HTTP timeout]" \
+        {-v,--verbose}"[turn on verbose logging]" \
         '(-): :->command'                       \
         '(-)*:: :->option-or-argument'          \
         && return 0;
