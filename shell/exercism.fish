965a3ac37a50f87232d248a54593e937a4485b76
John Goff <john.g@hiredhippo.ai>
2018-11-13 23:53:46 -0500

    prevent already matched arguments from being matched repeatedly

--- shell/exercism.fish
+++ shell/exercism.fish
@@ -1,12 +1,12 @@
 # Configure
-complete -f -c exercism -a "configure" -d "Writes config values to a JSON file."
+complete -f -c exercism -n "__fish_use_subcommand" -a "configure" -d "Writes config values to a JSON file."
 complete -f -c exercism -n "__fish_seen_subcommand_from configure" -s t -l token -d "Set token"
 complete -f -c exercism -n "__fish_seen_subcommand_from configure" -s w -l workspace -d "Set workspace"
 complete -f -c exercism -n "__fish_seen_subcommand_from configure" -s a -l api -d "set API base url"
 complete -f -c exercism -n "__fish_seen_subcommand_from configure" -s s -l show -d "show settings"
 
 # Download
-complete -f -c exercism -a "download" -d "Downloads and saves a specified submission into the local system"
+complete -f -c exercism -n "__fish_use_subcommand" -a "download" -d "Downloads and saves a specified submission into the local system"
 complete -f -c exercism -n "__fish_seen_subcommand_from download" -s e -l exercise -d "the exercise slug"
 complete -f -c exercism -n "__fish_seen_subcommand_from download" -s h -l help -d "help for download"
 complete -f -c exercism -n "__fish_seen_subcommand_from download" -s T -l team -d "the team slug"
@@ -14,33 +14,33 @@ complete -f -c exercism -n "__fish_seen_subcommand_from download" -s t -l track 
 complete -f -c exercism -n "__fish_seen_subcommand_from download" -s u -l uuid -d "the solution UUID"
 
 # Help
-complete -f -c exercism -a "help" -d "Shows a list of commands or help for one command"
+complete -f -c exercism -n "__fish_use_subcommand" -a "help" -d "Shows a list of commands or help for one command"
 complete -f -c exercism -n "__fish_seen_subcommand_from help" -a "configure download help open submit troubleshoot upgrade version workspace"
 
 # Open
-complete -f -c exercism -a "open" -d "Opens a browser to exercism.io for the specified submission."
+complete -f -c exercism -n "__fish_use_subcommand" -a "open" -d "Opens a browser to exercism.io for the specified submission."
 complete -f -c exercism -n "__fish_seen_subcommand_from open" -s h -l help -d "help for open"
 
 # Submit
-complete -f -c exercism -a "submit" -d "Submits a new iteration to a problem on exercism.io."
+complete -f -c exercism -n "__fish_use_subcommand" -a "submit" -d "Submits a new iteration to a problem on exercism.io."
 complete -f -c exercism -n "__fish_seen_subcommand_from submit" -s h -l help -d "help for submit"
 
 # Troubleshoot
-complete -f -c exercism -a "troubleshoot" -d "Outputs useful debug information."
+complete -f -c exercism -n "__fish_use_subcommand" -a "troubleshoot" -d "Outputs useful debug information."
 complete -f -c exercism -n "__fish_seen_subcommand_from troubleshoot" -s f -l full-api-key -d "display full API key (censored by default)"
 complete -f -c exercism -n "__fish_seen_subcommand_from troubleshoot" -s h -l help -d "help for troubleshoot"
 
 # Upgrade
-complete -f -c exercism -a "upgrade" -d "Upgrades to the latest available version."
+complete -f -c exercism -n "__fish_use_subcommand" -a "upgrade" -d "Upgrades to the latest available version."
 complete -f -c exercism -n "__fish_seen_subcommand_from help" -s h -l help -d "help for help"
 
 # Version
-complete -f -c exercism -a "version" -d "Outputs version information."
+complete -f -c exercism -n "__fish_use_subcommand" -a "version" -d "Outputs version information."
 complete -f -c exercism -n "__fish_seen_subcommand_from version" -s l -l latest -d "check latest available version"
 complete -f -c exercism -n "__fish_seen_subcommand_from version" -s h -l help -d "help for version"
 
 # Workspace
-complete -f -c exercism -a "workspace" -d "Outputs the root directory for Exercism exercises."
+complete -f -c exercism -n "__fish_use_subcommand" -a "workspace" -d "Outputs the root directory for Exercism exercises."
 complete -f -c exercism -n "__fish_seen_subcommand_from workspace" -s h -l help -d "help for workspace"
 
 # Options
