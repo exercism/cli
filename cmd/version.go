6067431dc195fd2e1050b95ae7aa597839d4622a
Katrina Owen <kytrinyx@github.com>
2018-11-18 13:43:35 -0700

    Bump version to 3.0.11

--- cmd/version.go
+++ cmd/version.go
@@ -9,7 +9,7 @@ import (
 
 // Version is the version of the current build.
 // It follows semantic versioning.
-const Version = "3.0.10"
+const Version = "3.0.11"
 
 // checkLatest flag for version command.
 var checkLatest bool
