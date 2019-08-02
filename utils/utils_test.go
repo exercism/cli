a81e36b348e111641710508877896702aba26753
Jamario Rankins <jamario.rankins@thoughtworks.com>
2019-08-02 09:44:47 -0600

    Fixing test

--- utils/utils_test.go
+++ utils/utils_test.go
@@ -3,7 +3,6 @@ package utils
 import (
 	"testing"
 
-	"github.com/exercism/cli/utils"
 	"github.com/stretchr/testify/assert"
 )
 
@@ -11,5 +10,5 @@ func TestRedact(t *testing.T) {
 	fakeToken := "1a11111aaaa111aa1a11111a11111aa1"
 	expected := "1a11*************************aa1"
 
-	assert.Equal(t, expected, utils.Redact(fakeToken))
+	assert.Equal(t, expected, Redact(fakeToken))
 }
