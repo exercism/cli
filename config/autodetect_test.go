874e1b70752ef8ac3892d34f7e21c7584c663ead
Chocopowwwa <chocopowwwa@gmail.com>
2020-04-24 19:50:48 +0700

    Add some tests

--- config/autodetect_test.go
+++ config/autodetect_test.go
@@ -40,10 +40,59 @@ func TestAutoDetect(t *testing.T) {
 				filepath.Join(exerciseDir, "bob.sh"),
 			},
 		},
+		{
+			"cpp",
+			[]string{"bob.c", "bob.h", "bob.cpp", "CMakeLists.txt"},
+			[]string{
+				filepath.Join(exerciseDir, "bob.h"),
+				filepath.Join(exerciseDir, "bob.cpp"),
+			},
+		},
+		{
+			"crystal",
+			[]string{"src/bob.cr", "spec/bob.cr", "src/bob.cpp"},
+			[]string{
+				filepath.Join(exerciseDir, "src/bob.cr"),
+			},
+		},
+		{
+			"dart",
+			[]string{"lib/bob.dart", "lib/bob.cr", "src/bob.cpp"},
+			[]string{
+				filepath.Join(exerciseDir, "lib/bob.dart"),
+			},
+		},
+		{
+			"elixir",
+			[]string{"lib/bob.ex", "lib/bob.cr", "src/bob_test.exs"},
+			[]string{
+				filepath.Join(exerciseDir, "lib/bob.ex"),
+			},
+		},
+		{
+			"go",
+			[]string{"bob.go", "bob_test.go", "bob_test.exs"},
+			[]string{
+				filepath.Join(exerciseDir, "bob.go"),
+			},
+		},
+		{
+			"haskell",
+			[]string{"src/Bob.hs", "src/bob_test.exs"},
+			[]string{
+				filepath.Join(exerciseDir, "src/Bob.hs"),
+			},
+		},
 	}
 
 	srcDir := filepath.Join(exerciseDir, "src")
 	os.Mkdir(srcDir, 0755)
+
+	specDir := filepath.Join(exerciseDir, "spec")
+	os.Mkdir(specDir, 0755)
+
+	libDir := filepath.Join(exerciseDir, "lib")
+	os.Mkdir(libDir, 0755)
 
 	// Arbitrary files
 	err = createFiles([]string{"readme.md"}, exerciseDir)
@@ -54,7 +103,7 @@ func TestAutoDetect(t *testing.T) {
 		assert.NoError(t, err)
 
 		solutionFiles, err := FindSolutions(defaultTrackGlobs, tc.Track, exerciseDir)
-		t.Logf("Running test for track %s", tc.Track)
+		t.Logf("Running AutoDetectTest for track %s", tc.Track)
 		assert.NoError(t, err)
 		assert.ElementsMatch(t, tc.Expected, solutionFiles)
 	}
