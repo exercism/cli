7a5f321d4de15ccbf6fff4c6e7192871eeddb876
Sascha Mann <git@mail.saschamann.eu>
2020-11-14 14:00:14 +0100

    Run exercism as command instead

--- cmd/handler-entrypoint.go
+++ cmd/handler-entrypoint.go
@@ -4,6 +4,8 @@ import (
 	"errors"
 	"fmt"
 	"log"
+	"os"
+	"os/exec"
 	"runtime"
 	"strings"
 
@@ -19,13 +21,24 @@ var handlerEntrypointCmd = &cobra.Command{
 	},
 }
 
+func exit(exitCode int) {
+	fmt.Println("\nPress ENTER to exit.")
+	_, err := fmt.Scanln()
+	if err != nil {
+		log.Fatal(err)
+	}
+	os.Exit(exitCode)
+}
+
 func dispatchURICommand(cmd *cobra.Command, args []string) error {
 	if runtime.GOOS != "windows" {
-		return errors.New("only supported on Windows")
+		fmt.Println("only supported on Windows")
+		exit(1)
 	}
 
 	if len(args) < 1 {
-		return errors.New("no arguments given")
+		fmt.Println("no arguments given")
+		exit(1)
 	}
 
 	params := strings.Split(args[0], ":")
@@ -34,22 +47,28 @@ func dispatchURICommand(cmd *cobra.Command, args []string) error {
 	switch params[0] {
 	case "download":
 		uuid := params[1]
-		fmt.Printf("Downloading %s...", uuid)
-		err := downloadCmd.RunE(cmd, []string{fmt.Sprintf("--uuid=%s", uuid)})
+		_cmd := exec.Command("exercism", "download", fmt.Sprintf("--uuid=%s", uuid))
+		_cmd.Stderr = os.Stderr
+		_cmd.Stdout = os.Stdout
+		err := _cmd.Run()
 		if err != nil {
-			log.Fatalf("download failed: %v", err)
+			exit(1)
 		}
-		return nil
+		exit(0)
 	case "configure":
-		if params[2] == "token" {
-			token := params[3]
-			fmt.Printf("Configuring token=%s...", token)
-			err := configureCmd.RunE(cmd, []string{fmt.Sprintf("--token=%s", token)})
+		if params[1] == "token" {
+			token := params[2]
+			//err := configureCmd.RunE(cmd, []string{fmt.Sprintf("--token=%s", token)})
+			_cmd := exec.Command("exercism", "configure", fmt.Sprintf("--token=%s", token))
+			_cmd.Stderr = os.Stderr
+			_cmd.Stdout = os.Stdout
+			err := _cmd.Run()
 			if err != nil {
-				log.Fatalf("configuring token failed: %v", err)
+				exit(1)
 			}
+			exit(0)
 		}
-		return nil
+		exit(1)
 	}
 
 	return errors.New("invalid command")
