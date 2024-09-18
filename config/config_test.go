268e882e392d63779658e056e7c1090113268908
Isaac Good <github@isaacgood.com>
2024-09-18 10:08:41 -0700

    Revert "Fix URL in no token error message (#1166)"
    This reverts commit 102aeb8f07d691b352412c8a80f7626656ae2f7e.

--- config/config_test.go
+++ config/config_test.go
@@ -10,11 +10,11 @@ func TestInferSiteURL(t *testing.T) {
 	testCases := []struct {
 		api, url string
 	}{
-		{"https://api.exercism.io/v1", "https://exercism.org"},
+		{"https://api.exercism.io/v1", "https://exercism.io"},
 		{"https://v2.exercism.io/api/v1", "https://v2.exercism.io"},
 		{"https://mentors-beta.exercism.io/api/v1", "https://mentors-beta.exercism.io"},
 		{"http://localhost:3000/api/v1", "http://localhost:3000"},
-		{"", "https://exercism.org"},           // use the default
+		{"", "https://exercism.io"},            // use the default
 		{"http://whatever", "http://whatever"}, // you're on your own, pal
 	}
 
