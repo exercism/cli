14f2349edc8cd1e2ad7393c14e17ceca06969ef9
Erik Schierboom <erik_schierboom@hotmail.com>
2025-05-30 14:51:19 +0200

    Bump version to 3.5.5 (#1194)

--- cmd/version.go
+++ cmd/version.go
@@ -9,7 +9,7 @@ import (
 
 // Version is the version of the current build.
 // It follows semantic versioning.
-const Version = "3.5.4"
+const Version = "3.5.5"
 
 // checkLatest flag for version command.
 var checkLatest bool
