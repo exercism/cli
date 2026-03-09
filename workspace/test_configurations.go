961a258dd9b2f162a2530ab7619454329e4577f9
András B Nagy <20251272+BNAndras@users.noreply.github.com>
2026-03-08 17:34:19 -0700

    Add `rescript` support

--- workspace/test_configurations.go
+++ workspace/test_configurations.go
@@ -250,6 +250,9 @@ var TestConfigurations = map[string]TestConfiguration{
 	"red": {
 		Command: "red {{test_files}}",
 	},
+	"rescript": {
+		Command: "npm run test",
+	},
 	"roc": {
 		Command: "roc test {{test_files}}",
 	},
